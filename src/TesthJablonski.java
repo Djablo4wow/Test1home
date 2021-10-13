38185292f4ef88a5df3ead31e6b97ebc4a296dec
Dawid Jablonski <davejablo420@wp.pl>
2021-10-13 23:34:26 +0200

    initial commit

--- src/TesthJablonski.java
+++ src/TesthJablonski.java
@@ -3,5 +3,6 @@ public class TesthJablonski
     public static void main(String[] args)
     {
         System.out.println("Pizza mmm pychotki");
+        System.out.println("Hawajska jest niezla");
     }
 }
